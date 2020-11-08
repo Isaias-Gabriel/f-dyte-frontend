acb1c9bf8f87aa291004bb7ae262009fe472c1b7
= <=>
2020-11-08 11:04:04 -0300

    commit

--- src/components/Profile/Queima/OriginalQueima.component.js
+++ src/components/Profile/Queima/OriginalQueima.component.js
@@ -1,194 +1,194 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
+// import React, { Component } from 'react';
+// import { Link } from 'react-router-dom';
+// import axios from 'axios';
 
-import IsLogged from '../IsLogged/IsLogged.component';
-import GoHome from '../GoHome/GoHome.component';
-import LogOut from '../LogOut/LogOut.component';
+// import IsLogged from '../IsLogged/IsLogged.component';
+// import GoHome from '../GoHome/GoHome.component';
+// import LogOut from '../LogOut/LogOut.component';
 
-import QueimaPosts from './QueimaPosts.component';
-import FollowUser from '../FollowUser/FollowUser.component';
+// import QueimaPosts from './QueimaPosts.component';
+// import FollowUser from '../FollowUser/FollowUser.component';
 
-import './QueimaStyles.css';
+// import './QueimaStyles.css';
 
-require('dotenv/config');
+// require('dotenv/config');
 
-export default class Queima extends Component {
+// export default class Queima extends Component {
 
-    constructor(props) {
-        super(props);
+//     constructor(props) {
+//         super(props);
 
-        this.handleChange = this.handleChange.bind(this);
+//         this.handleChange = this.handleChange.bind(this);
 
-        this.showRatingSlider = this.showRatingSlider.bind(this);
+//         this.showRatingSlider = this.showRatingSlider.bind(this);
 
-        this.showFollowButton = this.showFollowButton.bind(this);
+//         this.showFollowButton = this.showFollowButton.bind(this);
 
-        this.submitRate = this.submitRate.bind(this);
+//         this.submitRate = this.submitRate.bind(this);
 
-        this.state = {
-            evaluator: {
-                rate: {}
-            },
-            rateToSubmit: 0,
+//         this.state = {
+//             evaluator: {
+//                 rate: {}
+//             },
+//             rateToSubmit: 0,
 
-            canBeRated: false,
-        }
-    }
+//             canBeRated: false,
+//         }
+//     }
 
-    componentDidMount() {
-        axios.get(process.env.REACT_APP_SERVER_ADDRESS + '/complete_evaluator_info/' + this.props.match.params.username)
-            .then(response => {
-                this.setState({
-                    evaluator: response.data.evaluator,
-                    followersNumber: response.data.followersNumber,
-                }, () => {
-                    const formInfo = {
-                        profileUsername: this.props.match.params.username,
-                        sessionId: localStorage.getItem('e'),
-                    };
+//     componentDidMount() {
+//         axios.get(process.env.REACT_APP_SERVER_ADDRESS + '/complete_evaluator_info/' + this.props.match.params.username)
+//             .then(response => {
+//                 this.setState({
+//                     evaluator: response.data.evaluator,
+//                     followersNumber: response.data.followersNumber,
+//                 }, () => {
+//                     const formInfo = {
+//                         profileUsername: this.props.match.params.username,
+//                         sessionId: localStorage.getItem('e'),
+//                     };
 
-                    //if the user can rate the evaluator from the profile, it will return true, else, return false :v
-                    axios.post(process.env.REACT_APP_SERVER_ADDRESS + '/user_can_rate_or_follow_user', formInfo)
-                        .then(res => {
-                            this.setState({
-                                canBeRated: res.data.canBeRated,
-                                isFollowed: res.data.isFollowed,
-                            })
-                        })
-                        .catch(err => console.log(err));
+//                     //if the user can rate the evaluator from the profile, it will return true, else, return false :v
+//                     axios.post(process.env.REACT_APP_SERVER_ADDRESS + '/user_can_rate_or_follow_user', formInfo)
+//                         .then(res => {
+//                             this.setState({
+//                                 canBeRated: res.data.canBeRated,
+//                                 isFollowed: res.data.isFollowed,
+//                             })
+//                         })
+//                         .catch(err => console.log(err));
 
-                })
-            })
-            .catch(err => console.log(err));
+//                 })
+//             })
+//             .catch(err => console.log(err));
 
-    }
+//     }
 
-    handleChange = e => {
-        this.setState({
-            [ e.target.name ]: e.target.value
-        })
-    }
+//     handleChange = e => {
+//         this.setState({
+//             [ e.target.name ]: e.target.value
+//         })
+//     }
 
-    showRatingSlider() {
-        if(this.state.canBeRated) {
-            const { rateToSubmit } = this.state;
+//     showRatingSlider() {
+//         if(this.state.canBeRated) {
+//             const { rateToSubmit } = this.state;
 
-            return(
-                <div className="slidecontainer">
-                    <form id="rate-evaluator" onSubmit={this.submitRate}>
-                        <input 
-                            type="range"
-                            name="rateToSubmit"
+//             return(
+//                 <div className="slidecontainer">
+//                     <form id="rate-evaluator" onSubmit={this.submitRate}>
+//                         <input 
+//                             type="range"
+//                             name="rateToSubmit"
 
-                            min="0"
-                            max="5000000"
-                            value={rateToSubmit}
-                            onChange={this.handleChange}
+//                             min="0"
+//                             max="5000000"
+//                             value={rateToSubmit}
+//                             onChange={this.handleChange}
 
-                            id="myRange"
-                            className="slider"
-                        />
+//                             id="myRange"
+//                             className="slider"
+//                         />
 
-                        <p>Value: <span>{ Number(this.state.rateToSubmit / 1000000).toFixed(2) }</span></p>
-                        <button type="submit">Rate { this.state.evaluator.name }</button>
-                    </form>
-                </div>
-            )
-        }
-    }
+//                         <p>Value: <span>{ Number(this.state.rateToSubmit / 1000000).toFixed(2) }</span></p>
+//                         <button type="submit">Rate { this.state.evaluator.name }</button>
+//                     </form>
+//                 </div>
+//             )
+//         }
+//     }
 
-    showFollowButton() {
-        const { name, username } = this.state.evaluator;
-        const { followersNumber, isFollowed } = this.state;
+//     showFollowButton() {
+//         const { name, username } = this.state.evaluator;
+//         const { followersNumber, isFollowed } = this.state;
 
-        if(username && name && (typeof isFollowed !== typeof undefined) && 
-            (typeof followersNumber !== typeof undefined)) {
-            return(
-                <FollowUser
-                    username={username}
-                    name={name}
-                    isFollowed={isFollowed}
-                    followersNumber={followersNumber}
-                />
-            )
-        }
+//         if(username && name && (typeof isFollowed !== typeof undefined) && 
+//             (typeof followersNumber !== typeof undefined)) {
+//             return(
+//                 <FollowUser
+//                     username={username}
+//                     name={name}
+//                     isFollowed={isFollowed}
+//                     followersNumber={followersNumber}
+//                 />
+//             )
+//         }
         
-    }
+//     }
     
-    submitRate(e) {
-        e.preventDefault();
+//     submitRate(e) {
+//         e.preventDefault();
 
-        let formInfo = {
-            rateToSubmit: this.state.rateToSubmit / 1000000,
-            evaluatedId: this.state.evaluator._id,
-            sessionId: localStorage.getItem('e'),
-        }
+//         let formInfo = {
+//             rateToSubmit: this.state.rateToSubmit / 1000000,
+//             evaluatedId: this.state.evaluator._id,
+//             sessionId: localStorage.getItem('e'),
+//         }
 
-        axios.post(process.env.REACT_APP_SERVER_ADDRESS + '/update_user_rate', formInfo)
-            .then(res => {
-                alert('Rated!');
+//         axios.post(process.env.REACT_APP_SERVER_ADDRESS + '/update_user_rate', formInfo)
+//             .then(res => {
+//                 alert('Rated!');
                 
-                this.setState({
-                    evaluator: res.data,
-                    canBeRated: false,
-                })
-            })
-            .catch(err => console.log(err));
-    }
+//                 this.setState({
+//                     evaluator: res.data,
+//                     canBeRated: false,
+//                 })
+//             })
+//             .catch(err => console.log(err));
+//     }
 
-    render() {
-        const { name, username, rate, rateNumber } = this.state.evaluator;
+//     render() {
+//         const { name, username, rate, rateNumber } = this.state.evaluator;
         
-        return(
-            <div id="profile-container">
-                <IsLogged/>
-                <GoHome/>
-                <LogOut/>
+//         return(
+//             <div id="profile-container">
+//                 <IsLogged/>
+//                 <GoHome/>
+//                 <LogOut/>
 
-                <div>
-                    <h1>
-                        { name }
-                    </h1>
+//                 <div>
+//                     <h1>
+//                         { name }
+//                     </h1>
                     
-                    <p>
-                        @{ username }
-                    </p>
+//                     <p>
+//                         @{ username }
+//                     </p>
 
-                    <span>
-                        Rate: { Number(rate.$numberDecimal).toFixed(2) }
-                    </span>
+//                     <span>
+//                         Rate: { Number(rate.$numberDecimal).toFixed(2) }
+//                     </span>
                     
-                    <p>
-                        Number of rates: { rateNumber }
-                    </p>
-                </div>
+//                     <p>
+//                         Number of rates: { rateNumber }
+//                     </p>
+//                 </div>
                 
-                {
-                    this.showRatingSlider()
-                }
+//                 {
+//                     this.showRatingSlider()
+//                 }
 
-                {
-                    this.showFollowButton()
-                }
+//                 {
+//                     this.showFollowButton()
+//                 }
 
-                <Link to={"/profile/" + this.props.match.params.username}>
-                    Feed
-                </Link>
+//                 <Link to={"/profile/" + this.props.match.params.username}>
+//                     Feed
+//                 </Link>
 
-                <Link to={"/profile/" + this.props.match.params.username + "/belle"}>
-                    Belle
-                </Link>
+//                 <Link to={"/profile/" + this.props.match.params.username + "/belle"}>
+//                     Belle
+//                 </Link>
                 
-                <section>
-                    <div id="add-post">
-                        <QueimaPosts
-                            username={ this.props.match.params.username }
-                        />  
+//                 <section>
+//                     <div id="add-post">
+//                         <QueimaPosts
+//                             username={ this.props.match.params.username }
+//                         />  
                         
-                    </div>
-                </section>
-            </div>
-        );
-    }
-}
+//                     </div>
+//                 </section>
+//             </div>
+//         );
+//     }
+// }
